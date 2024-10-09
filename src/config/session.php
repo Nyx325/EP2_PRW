99181ebb3a0fd0735ab80554b106df85ab370c25
Omar Román <omar.roman.2828@gmail.com>
2024-10-09 16:38:49 -0600

    funcion login con mysql

--- src/config/session.php
+++ src/config/session.php
@@ -1,35 +1,50 @@
+<?php include './database.php' ?>
 <?php
 // Iniciar la sesión
 session_start();
 
-// Función para login()
-function login()
+function login($conn)
 {
+  $response = ['status' => 'error', 'message' => ''];
   $data = json_decode(file_get_contents('php://input'), true);
 
   if (!isset($data['user']['userName']) || !isset($data['user']['password'])) {
-    echo json_encode(['status' => 'error', 'message' => 'Por favor, proporciona un nombre de usuario y una contraseña.']);
+    $response['message'] = 'Por favor, proporciona un nombre de usuario y una contraseña.';
+    echo json_encode($response);
     return;
   }
 
   $username = $data['user']['userName'];
   $password = $data['user']['password'];
 
-  // Validar las credenciales
-  if ($username === 'root' && $password === '123') {
-    $_SESSION['username'] = $username;
-    echo json_encode(['status' => 'success', 'message' => 'Sesión iniciada', 'username' => $_SESSION['username']]);
-  } else {
-    echo json_encode(['status' => 'error', 'message' => 'Credenciales incorrectas']);
+  // Preparar la consulta para evitar inyección SQL
+  $stmt = $conn->prepare("SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?");
+  $stmt->bind_param("ss", $username, $password);
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  if ($result->num_rows === 0) {
+    $response['message'] = 'Credenciales incorrectas';
+    echo json_encode($response);
+    return;
   }
+
+  $row = $result->fetch_assoc();
+  $_SESSION['username'] = $username;
+  $_SESSION['tipo'] = $row['tipo'];
+
+  $response['status'] = 'success';
+  $response['message'] = 'Sesión iniciada';
+  echo json_encode($response);
 }
+
 
 function isUserLogged()
 {
   if (isset($_SESSION['username'])) {
     echo json_encode(['status' => 'success', 'username' => $_SESSION['username']]);
   } else {
-    echo json_encode(['status' => 'error', 'message' => 'No hay sesión activa']);
+    echo json_encode(['status' => 'error', 'message' => 'No hay sesion activa']);
   }
 }
 
@@ -37,17 +52,17 @@ function logOut()
 {
   session_unset();
   session_destroy();
-  echo json_encode(['message' => 'Sesión cerrada']);
+  echo json_encode(['message' => 'Sesion cerrada']);
 }
 
-function main()
+function main($conn)
 {
   // Obtener el método HTTP
   $method = $_SERVER['REQUEST_METHOD'];
 
   switch ($method) {
     case 'POST':
-      login();
+      login($conn);
       break;
 
     case 'GET':
@@ -64,4 +79,4 @@ function main()
   }
 }
 
-main();
+main($conn);
